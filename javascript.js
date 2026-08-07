3c231eca5c510d53f2a9b6e5b5806148c45ed36a
Heshmat wick <308167658+heshmatwick-ops@users.noreply.github.com>
2026-08-08 03:03:25 +0330

    added the game round func that uses 2 previous funcs to decide the winner of the round.

--- javascript.js
+++ javascript.js
@@ -13,11 +13,69 @@ function getComputerChoice() {
     }
 
     else {
-    compchoice = "ROCK"
+    compchoice = "rock"
     }
 
     return compchoice;
 }
 
+function getHumanChoice() {
 
-console.log(getComputerChoice());
+    let playerchoice = prompt("Choose rock, paper or scissors")
+    return playerchoice;
+}
+
+function gameround(player = getHumanChoice(), comp = getComputerChoice()) {
+
+    let winner = 0;
+
+    if (player == comp) {
+
+        console.log("your tied!");
+        winner = "tied"
+    }
+
+    else if (player == "rock" && comp == "paper" ) {
+
+        console.log("comp wins!");
+        winner = "comp"
+    }
+
+    else if (player == "rock" && comp == "scissors" ) {
+
+        console.log("player wins!");
+        winner = "player"
+    }
+
+    else if (player == "scissors" && comp == "rock" ) {
+
+        console.log("comp wins!");
+        winner = "comp"
+    }
+
+    else if (player == "scissors" && comp == "paper" ) {
+
+        console.log("player wins!");
+        winner = "player"
+    }
+
+    else if (player == "paper" && comp == "rock" ) {
+
+        console.log("comp wins!");
+        winner = "comp"
+    }
+
+    else if (player == "paper" && comp == "scissors" ) {
+
+        console.log("player wins!");
+        winner = "player"
+    }
+
+    return winner;
+}
+
+console.log(gameround());
+
+//console.log(getHumanChoice());
+
+//console.log(getComputerChoice());
